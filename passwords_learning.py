84fbf4f394c2e8cec0a6459f0928cc5d6bca0c27
jonathan YHWH <alphajorider@gmail.com>
2023-05-29 14:22:15 +0200

    finish

--- passwords_learning.py
+++ passwords_learning.py
@@ -1,73 +1,60 @@
-import re
 import asyncio
+import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
-# Fonction pour évaluer la sécurité d'un mot de passe
-def evaluate_password_security(password):
-    security_level = 0
 
-    # Vérification des critères de sécurité
-    if re.search(r"[A-Z]", password):
-        security_level += 1
-    if re.search(r"[a-z]", password):
-        security_level += 1
-    if re.search(r"\d", password):
-        security_level += 1
-    if re.search(r"[#?!@$%^&*-]", password):
-        security_level += 1
-
-    return security_level
-
-# Fonction pour lire les mots de passe depuis un fichier de manière asynchrone
 async def read_passwords_from_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         passwords = file.read().splitlines()
-        passwords = [password.strip() for password in passwords]
-        return passwords
+    return passwords
 
-# Fonction pour évaluer la sécurité des mots de passe en utilisant le machine learning
-async def evaluate_password_security_ml(passwords):
-    # Prétraitement des mots de passe en enlevant les caractères spéciaux
+
+async def preprocess_passwords(passwords):
     preprocessed_passwords = [re.sub(r"[^A-Za-z0-9]+", "", password) for password in passwords]
+    return preprocessed_passwords
+
+
+async def generate_regex():
+    # Charger les mots de passe depuis le fichier
+    passwords = await read_passwords_from_file("passwords.txt")
+
+    # Prétraitement des mots de passe
+    preprocessed_passwords = await preprocess_passwords(passwords)
 
     # Création des vecteurs de caractéristiques en utilisant TF-IDF
     vectorizer = TfidfVectorizer()
     features = vectorizer.fit_transform(preprocessed_passwords)
 
     # Clustering des mots de passe similaires en utilisant K-means
-    kmeans = KMeans(n_clusters=5)  # Spécifiez le nombre de clusters souhaité
+    kmeans = KMeans(n_clusters=1)  # Spécifiez le nombre de clusters souhaité
     kmeans.fit(features)
 
-    # Rapport de sécurité des mots de passe
-    report = []
-    report.append("Rapport de sécurité des mots de passe\n")
+    # Récupération des mots de passe similaires dans le cluster
+    cluster_passwords = [passwords[j] for j in range(len(passwords)) if kmeans.labels_[j] == 0]
 
-    for i in range(kmeans.n_clusters):
-        cluster_passwords = [passwords[j] for j in range(len(passwords)) if kmeans.labels_[j] == i]
-        report.append(f"Cluster {i+1} - Nombre de mots de passe : {len(cluster_passwords)}")
-        report.extend(cluster_passwords)
-        report.append("")
+    # Génération du regex à partir des mots de passe similaires
+    regex = generate_regex_from_passwords(cluster_passwords)
 
-    return report
+    return regex
 
-# Fonction principale asynchrone
+
+def generate_regex_from_passwords(passwords):
+    # Implémentez ici votre algorithme pour générer le regex à partir des mots de passe donnés
+    # ...
+    regex = ""
+
+    return regex
+
+
 async def main():
-    # Lecture des mots de passe du fichier generated_passwords.txt de manière asynchrone
-    generated_passwords = await read_passwords_from_file("generated_passwords.txt")
+    regex = await generate_regex()
 
-    # Lecture des mots de passe du fichier passwords.txt de manière asynchrone
-    existing_passwords = await read_passwords_from_file("passwords.txt")
+    # Écriture du regex dans un fichier
+    with open("generated_regex.txt", "w") as file:
+        file.write(regex)
 
-    # Évaluation de la sécurité des mots de passe en utilisant le machine learning de manière asynchrone
-    generated_report = await evaluate_password_security_ml(generated_passwords)
-    existing_report = await evaluate_password_security_ml(existing_passwords)
+    print("Regex generated and saved successfully.")
 
-    # Écriture du rapport dans un fichier
-    with open("password_security_report.txt", "w", encoding="utf-8") as file:
-        file.write("\n".join(generated_report + existing_report))
 
-    print("Le rapport de sécurité des mots de passe a été généré dans le fichier password_security_report.txt.")
-
-# Exécution de la fonction principale asynchrone
 asyncio.run(main())
